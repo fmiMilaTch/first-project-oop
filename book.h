a701e0d8b19a3c941c92a40d59d9e609eca0ac05
fmiMilaTch <104836126+fmiMilaTch@users.noreply.github.com>
2022-05-11 22:18:10 +0300

    Update book.h
    tired2

--- book.h
+++ book.h
@@ -4,6 +4,8 @@
 const unsigned ISBN_LEN=13;
 const unsigned MIN_RATING=1;
 const unsigned MAX_RATING=5;
+
+class Library;
 class Book{
     char* author;
     unsigned authorLenght;
@@ -17,15 +19,20 @@ class Book{
     char* ISBN;
 
     void setText(const char*, char*, unsigned&);
+    void setFileName(const char*, char*, unsigned&);//pochti identichno na setText, mozge da napravia edna funkcia s default stoinost;;
     void setISBN(const char*);
     void setRating(const unsigned&);
+    void changeText(const char*, char*, unsigned&);
+    void changeFileName(const char*, char*, unsigned&);
+    void changeISBN(const char*);
+    void changeRating(const unsigned&);
     void free();
 
 public:
+    friend class Library;
 
 
-
-    bool onlyNumbers(const char*) const;//uh toia const...i char*...
+    bool onlyNumbers(const char*) const;//uh toia const...i char*...// obikn funkcia
     //bool appropLenght(const char*)const;//dali da ne gi napravia friend?//a ne shto ne prosto strlen^^;
     //bool validFileName(const char*) const//triabva li mi takova neshto?
 
@@ -37,11 +44,12 @@ public:
     const unsigned getRating() const;
     const char* getISBN() const;
 
-    Book(const char* newAuthor, const char* newTitle, const char* newFile, const char* newDesc, const unsigned& newRating, const char* newISBN);
-    //does it need a move constructor??
-    Book(char* &&newAuthor, char* &&newTitle, char* &&newFile, char* &&newDesc, unsigned&& newRating, char* &&newISBN);
+    Book(const char* newAuthor="Unknown", const char* newTitle="Unknown", const char* newFile="Unknown.txt", const char* newDesc="Unknown", const unsigned& newRating=0, const char* newISBN="0000000000000");
     Book(const Book& other);
     Book(Book&& other);
+
+    friend std::ostream& operator<<(std::ostream&, const Book&) const;
+    friend std::istream& operator>>(std::istream&, Book&);//sega stana slozgnichko...
 
     ~Book();
 };
