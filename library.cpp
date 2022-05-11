16c9e24456e807e594fce447e6740f6e02554bd0
fmiMilaTch <104836126+fmiMilaTch@users.noreply.github.com>
2022-05-11 22:21:14 +0300

    Update library.cpp
    tired3

--- library.cpp
+++ library.cpp
@@ -1,1 +1,15 @@
 #include "library.h"
+
+Library::Library(){
+    numberOfBooks=0;
+    capacity=50;
+    bookListFile=new char[strlen("defaultBookListFile.txt")+1];
+    strcpy(bookListFile,"defaultBookListFile.txt");
+    booksByTitle=new Book* [capacity];
+    booksByISBN=new Book* [capacity];
+    booksByAuthor=new Book* [capacity];
+    booksByRating=new Book* [capacity];
+}
+Library::Library(const char* libraryFile, const unsigned& BooksNumber){
+
+}
