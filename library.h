9fe097dd3549dab7214ba33aeb65d7c23c845d74
fmiMilaTch <104836126+fmiMilaTch@users.noreply.github.com>
2022-05-12 23:31:18 +0300

    Update library.h

--- library.h
+++ library.h
@@ -1,9 +1,10 @@
 #include "book.h"
+const unsigned DEFAULT_CAPACITY=50;
 class Library{
-    Book** booksByTitle;//da se polzwa kato glavnia shablon za prenaregdane na ostanalite spisuci
-    Book** booksByISBN;
-    Book** booksByAuthor;
-    Book** booksByRating;
+    Book** booksByTitle;//byisbn
+    Book** booksByISBN;//da se polzwa kato glavnia shablon za prenaregdane na ostanalite spisuci
+    Book** booksByAuthor;//bytitle
+    Book** booksByRating;//bytitle
     unsigned numberOfBooks;
     unsigned capacity;
     char* bookListFile;
@@ -17,8 +18,13 @@ class Library{
     void sortBooksByISBN();
     void sortBooksByAuthor();
     void sortBooksByRating();
-    void updateBookListFile(const char*);//da se paziat po isbn
-    bool doesTextMatch(const char*, const char*) const;// po dobre da e friend
+    unsigned findNewBookISBNPosition(const char*);
+    void moveDownByOneFromPosition(const unsigned&);//neka da e prosto sus swapove ot kraia do position
+    void moveUpByOneFromPosition(const unsigned&);//same thing no ot position do bottom
+    bool doesISNBExist(const char*);
+    bool isBoolListFileUpdated=false;
+    void updateBookListFile();//da se paziat po isbn
+    bool doesTextMatch(const char*, const char*) const;// po dobre da e friend//triabva li mi nakraia?, abstrakcia e ig, no triabva da napravia oshte 2 takiva togava, koeto si e prosto strcmp:/
     void free();
 public:
 
